71dab3ba9b41fcf141d5cd40ca5b98d800023a27
Elon Zito <simsketch@gmail.com>
2025-01-03 20:17:42 -0500

    Auth debugging

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,22 +2,14 @@
 const nextConfig = {
   // Tell Next.js we're behind a proxy
   poweredByHeader: false,
-  env: {
-    NEXT_PUBLIC_URL: process.env.NODE_ENV === 'production' 
-      ? 'https://zerodaybeta.betwixtai.com' 
-      : 'http://localhost:3000'
-  },
   // Configure how Next.js should handle the host header and protocol
+  basePath: process.env.NODE_ENV === 'production' ? '' : '',
   assetPrefix: process.env.NODE_ENV === 'production' 
     ? 'https://zerodaybeta.betwixtai.com' 
     : '',
   // Trust the proxy's headers
-  serverRuntimeConfig: {
-    trustProxy: true
-  },
-  // Override the hostname and protocol in production
   experimental: {
-    trustHostHeader: true,
+    forwardClientHeaders: true
   }
 }
 
